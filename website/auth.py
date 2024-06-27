2721a1fbe17f97d8a34322915a82bf00099b363a
ThyroneGamit <19193@my.sanctamaria.school.nz>
2024-06-27 13:10:41 +1200

    database models

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint,render_template, request
+from flask import Blueprint,render_template, request, flash
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   ##means from __init__.py import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth=Blueprint('auth',__name__)
 
@@ -13,8 +18,26 @@ def login():
 def logout():
     return"<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_Name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(first_Name) < 2:
+            flash('First name must be greater than 1 characters.', category='error')
+        elif len(password1) < 7:
+            flash('Passwords don\'t match.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        else:
+            flash('Account created!', category='success')
+            pass
+        
     return render_template("signup.html")
 
 
