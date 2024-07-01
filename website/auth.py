d165dc3eda561077814fd8ed13c87acc695bbd5d
ThyroneGamit <19193@my.sanctamaria.school.nz>
2024-07-02 11:54:49 +1200

    creating new user accounts

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template, request, flash
+from flask import Blueprint,render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db   ##means from __init__.py import db
@@ -22,21 +22,24 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        first_Name = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
         if len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
-        elif len(first_Name) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 characters.', category='error')
         elif len(password1) < 7:
             flash('Passwords don\'t match.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
-            pass
+            return redirect(url_for('views.home'))
         
     return render_template("signup.html")
 
