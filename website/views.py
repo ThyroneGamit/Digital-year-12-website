04f166e2e20581c7589a1e257bc5beae13842f22
ThyroneGamit <19193@my.sanctamaria.school.nz>
2024-08-05 15:11:50 +1200

    adding comments
    added comments for some of the parts

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+### import from modules###
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
@@ -5,22 +6,24 @@ from . import db
 import json
 
 views = Blueprint('views', __name__)
-
+#home route ( Must login )
 @views.route('/home')
 @login_required
 def home():
     return render_template("home.html", user=current_user)
-
+#statistics route
 @views.route('/stat')
 @login_required
 def stat():
     return render_template("stat.html", user=current_user)
-
+#Homepage route 
+@views.route('/')
 @views.route('/home1')
 def home1():
     return render_template("home1.html", user=current_user)
 
-
+#Video clips route 
+#notes in video clips
 @views.route('/clips', methods=['GET', 'POST'])
 @login_required
 def notes():
@@ -37,7 +40,7 @@ def notes():
 
     return render_template("clips.html", user=current_user)
 
-
+#notes route/ deleting Notes
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
     note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
